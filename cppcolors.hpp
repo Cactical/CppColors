970eee2fead9abaeed9b3eec93e38e1efe0e6c99
Cactical <mljohnsonpuff@gmail.com>
2022-11-19 23:17:26 -0600

    Addition of: CppColors header file (version 1)

---
+++ cppcolors.hpp
@@ -0,0 +1,20 @@
+/*
+Version 1
+Cpp Colors -- by Cactical
+A file with asci escape colors codes mapped to variables that can be used to color text.
+
+Made with love :)
+
+This file contains variables with colors that you can use to color your strings when printing them.
+ex. std::cout << blue << "example";
+The text and every other text you print will stay on that color, unless you put it on a different color.
+*/
+std::string black = "\u001b[30m";
+std::string red = "\u001b[31m";
+std::string green = "\u001b[32m";
+std::string yellow = "\u001b[33m";
+std::string blue = "\u001b[34m";
+std::string magenta = "\u001b[35m";
+std::string cyan = "\u001b[36m";
+std::string white = "\u001b[37m";
+
